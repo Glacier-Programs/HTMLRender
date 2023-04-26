a06e91bfeb167d399018e7ba42b99cec5348ab8f
Jared Dewey <guy4penguins@gmail.com>
2023-04-25 22:08:28 -0400

    Changed the name

--- HTMLRender/src/render/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod window_state;
-pub mod vertex;
-pub mod component_mesh;
--- HTMLRender/target/debug/deps/HTMLRender-41fa5aac5739ddf1.d
+++
@@ -1,7 +0,0 @@
-/Users/jareddewey/Documents/Coding/HTMLRender/HTMLRender/HTMLRender/target/debug/deps/HTMLRender-41fa5aac5739ddf1.rmeta: src/main.rs src/render/mod.rs src/render/window_state.rs
-
-/Users/jareddewey/Documents/Coding/HTMLRender/HTMLRender/HTMLRender/target/debug/deps/HTMLRender-41fa5aac5739ddf1.d: src/main.rs src/render/mod.rs src/render/window_state.rs
-
-src/main.rs:
-src/render/mod.rs:
-src/render/window_state.rs:
