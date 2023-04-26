df893d041a890f274916a9a84895963ab1de2db4
Jared Dewey <guy4penguins@gmail.com>
2023-04-25 21:38:31 -0400

    -a

---
+++ HTMLRender/src/components.rs
@@ -0,0 +1,12 @@
+/*
+ * Components:
+ *     - Rectangle: A rectangle on screen
+ *     - TextBox: Just text with a background
+ *     - Button: Can be clicked
+ */
+
+pub enum Component{
+    Rectangle,
+    TextBox,
+    Button
+}
